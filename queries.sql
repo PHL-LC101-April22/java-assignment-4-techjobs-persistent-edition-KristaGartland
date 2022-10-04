c69a753a68b46a631f5166ac18abae33b5ea00d4
Krista Gartland <Randomflyingobject@yahoo.com>
2022-10-04 17:44:57 -0500

    Completed part 2

--- queries.sql
+++ queries.sql
@@ -1,6 +1,13 @@
 -- Part 1: Test it with SQL
+-- Table Job has 4 Columns.First is Id which is int, also a primary key.
+-- Second is employer which is varchar(255). Third and fourth is name and skills which are also varchar(225).
 
 -- Part 2: Test it with SQL
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
+
+
 
 -- Part 3: Test it with SQL
 
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,30 @@
 package org.launchcode.techjobs.persistent.models;
 
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
 
+    @NotBlank(message = "Location cannot be blank.")
+    @Size(min = 3, max = 60, message = "Location must be between 3 and 60 characters")
+    private String location;
+
+
+    public Employer(String location){
+        this.location = location;
+    }
+
+    public Employer(){}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
